2d1624c1ad2139fbe05ea23ed13e31e204f2706c
abraham-yusuf <1703020028@students.unis.ac.id>
2021-06-23 12:57:00 +0000

    init strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8fd937e1d3207bae5572d8ac657d095f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '12640972-6641-47d1-8950-40406e0b5b74'
+};
